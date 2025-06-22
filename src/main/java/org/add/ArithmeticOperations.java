fb08520210f28c25d9d6484fd4c527c725cb20dc
TruptiArade <truptiarade1414@gmail.com>
2025-06-22 12:57:22 +0530

    Moduler Operator modified

---
+++ src/main/java/org/add/ArithmeticOperations.java
@@ -0,0 +1,34 @@
+package org.add;
+
+public class ArithmeticOperations
+{
+
+    public  int adds(int a, int b)
+    {
+
+        return a+b;
+    }
+
+    public double sub(double no1, double no2)
+    {
+
+        return no1-no2;
+    }
+
+    public int mul(int m, int n)
+    {
+
+        return  m * n;
+    }
+
+    public double div(double num1, double num2)
+    {
+        return num1/num2;
+    }
+
+
+    public double modulename(double x, double y)
+    {
+        return x%y;
+    }
+}
---
+++ src/test/java/org/add/ArithmeticOperationsTest.java
@@ -0,0 +1,57 @@
+package org.add;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArithmeticOperationsTest
+{
+
+    @Test
+    public void addTest()
+    {
+        ArithmeticOperations c =new ArithmeticOperations();
+        int actualresult = c.adds(10,30);
+        System.out.println(actualresult);
+        assertEquals(40,actualresult);
+    }
+
+    @Test
+    public void subTest()
+    {
+        ArithmeticOperations ca =new ArithmeticOperations();
+        double subresult = ca.sub(80.0,70.0);
+        System.out.println( subresult);
+        assertEquals(10.0,subresult);
+    }
+
+    @Test
+    public void mulTest()
+    {
+        ArithmeticOperations cm = new ArithmeticOperations();
+        int mulresult = cm.mul(8,6);
+        System.out.println(mulresult);
+        assertEquals(48,mulresult);
+
+    }
+
+    @Test
+    public void divTest()
+    {
+        ArithmeticOperations cd = new ArithmeticOperations();
+        double  divresult = cd.div(25.0,5.0);
+        System.out.println(divresult);
+        assertEquals(5.0,divresult);
+    }
+
+    @Test
+    public void moduleTest()
+    {
+        ArithmeticOperations mod = new ArithmeticOperations();
+        double modresult = mod.modulename(8.0,2.0);
+        System.out.println(modresult);
+        assertEquals(0.0,modresult);
+    }
+
+
+}
